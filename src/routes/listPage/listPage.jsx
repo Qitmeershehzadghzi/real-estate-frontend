d3b61f6ecf6cc98e49a50368c4ed928fb4ea414a
Qitmeershehzadghzi <qitmeershehzad@gmail.com>
2026-05-14 22:13:05 +0500

    in list page add all dummy data and make card and fetch components

--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
-import { listData } from '../../lib/dummydata'
-import './listPage.scss'
-import Filter from '../../components/filter/Filter'
-import Card from '../../components/card/Card'
-const listPage = () => {
-  const data =listData
+import React from "react";
+import { listData } from "../../lib/dummydata";
+import "./listPage.scss";
+import Filter from "../../components/filter/Filter";
+import Card from "../../components/card/Card";
+
+const ListPage = () => {
+  const data = listData;
+
   return (
-    <div className='listpage'>
+    <div className="listPage">
       <div className="listContainer">
         <div className="wrapper">
-            <Filter/>
-{data.map(item=>(<Card key={item.id} item={item}/>))}
-
+          <Filter />
+          {data.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
         </div>
       </div>
+
       <div className="mapContainer">mapcontainer</div>
     </div>
-  )         
-}
+  );
+};
 
-export default listPage
+export default ListPage;
